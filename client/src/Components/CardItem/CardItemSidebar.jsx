212d62b1a3a2efb2118c2700e63a6cb3a3244079
Tony <anthonymessagesab@gmail.com>
2023-02-21 19:30:57 -0400

    responsividad para el sidebar item

--- client/src/Components/CardItem/CardItemSidebar.jsx
+++ client/src/Components/CardItem/CardItemSidebar.jsx
@@ -3,29 +3,32 @@ import "./CardItem.css";
 
 export const CardItemSidebar = ({ type }) => {
   return (
-    <div className="col-12">
-      <div className="position-relative">
-        <div className="card-image position-relative">
-          <img
-            src="https://picsum.photos/200/300"
-            className="content-card-main-banner"
-            alt="One Piece"
-          />
+    <div className="col">
+      <div className="card-inner">
+        <div className="position-relative">
+          <div className="card-image position-relative">
+            <img
+              src="https://picsum.photos/200/300"
+              className="content-card-main-banner"
+              alt="One Piece"
+            />
 
-          <span
-            className={`badge rounded-pill ${type} position-absolute pill text-uppercase fw-bold`}
-          >
-            {type}
-          </span>
-          <div className="demography text-white position-absolute bottom-0 text-center w-100">
-            Seinen
+            <span
+              className={`badge rounded-pill ${type} position-absolute pill text-uppercase fw-bold`}
+            >
+              {type}
+            </span>
+            <div className="demography text-white position-absolute bottom-0 text-center w-100">
+              Seinen
+            </div>
           </div>
-        </div>
 
-        <p className="title-card-main text-center mt-3">
-          One Piece efffffffe fe fe
-        </p>
+          <p className="title-card-main text-center mt-3">
+            One Piece efffffffe fe fe
+          </p>
+        </div>
       </div>
+
       <div></div>
     </div>
   );
