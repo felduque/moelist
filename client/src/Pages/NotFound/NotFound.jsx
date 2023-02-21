17fc8712a54c59330d9f608a8dff22d3d5a9cb1a
Tony <anthonymessagesab@gmail.com>
2023-02-21 19:37:48 -0400

    responsividad para notFound

--- client/src/Pages/NotFound/NotFound.jsx
+++ client/src/Pages/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ import "./NotFound.css";
 export const NotFound = () => {
   return (
     <section className="container-fluid not_found_container">
-      <div className="not_found_wrapper d-flex flex-column text-white justify-content-center">
+      <div className="not_found_wrapper d-flex flex-column text-white justify-content-center align-items-center">
         <img
           src="../../../public/not-found-error.svg"
           alt="Pagina no encontrada"
