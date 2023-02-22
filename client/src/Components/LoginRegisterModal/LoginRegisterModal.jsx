02f0d0c8a12a9882d3cc72ad43715bac14887eab
Tony <anthonymessagesab@gmail.com>
2023-02-22 18:53:04 -0400

    Formularios para Login & Registro

---
+++ client/src/Components/LoginRegisterModal/LoginRegisterModal.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { LoginForm } from "../LoginForm/LoginForm";
+import { IoIosClose } from "react-icons/io";
+import "./LoginRegisterModal.css";
+import { RegisterForm } from "../RegisterForm/RegisterForm";
+
+export const LoginRegisterModal = ({ formType, setFormType }) => {
+  return (
+    <div
+      className="modal fade"
+      id="modal"
+      aria-labelledby="modal"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog bg-dark">
+        <div className="modal-content bg-dark text-white">
+          <div class="modal-header ">
+            <h5 class="modal-title fs-2 form-title">{formType}</h5>
+            <IoIosClose
+              type="button"
+              className="btn p-0 text-white fs-1"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            />
+          </div>
+          {formType === "login" ? (
+            <LoginForm setFormType={setFormType} />
+          ) : (
+            <RegisterForm setFormType={setFormType} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
