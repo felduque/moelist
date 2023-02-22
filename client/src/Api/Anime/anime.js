98dc942350208e2f5dc875f6f8c43c780efaf6bb
Tony <anthonymessagesab@gmail.com>
2023-02-22 14:22:41 -0400

    conexion a backend

--- client/src/Api/Anime/anime.js
+++ client/src/Api/Anime/anime.js
@@ -1,3 +1,11 @@
 import axios from "axios";
 
-// export const getAnime = (id) => {
+export const getAnimes = async () => {
+  const response = await axios.get(`https://apix.moelist.online/animes`);
+  return response;
+};
+
+export const getAnimeById = async (id) => {
+  const response = await axios.get(`https://apix.moelist.online/anime/${id}`);
+  return response;
+};
