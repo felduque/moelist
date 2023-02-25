3dd203b2967980414b5e412e70ed23e604d4d142
Tony <anthonymessagesab@gmail.com>
2023-02-25 18:09:55 -0400

    se agrega un componente para ordenar

--- client/src/Components/Explorador/ExploradorOrder/ExploradorOrder.jsx
+++ client/src/Components/Explorador/ExploradorOrder/ExploradorOrder.jsx
@@ -1,32 +1,29 @@
 import React, { useContext, useEffect } from "react";
-import { useState } from "react";
 import { getAnimes } from "../../../Api/Anime/anime";
 import { ExploradorContext } from "../../../utils/context/ExploradorContext";
 
 export const ExploradorOrder = () => {
-  const [orden, setOrden] = useState("DESC");
-  const { setItems } = useContext(ExploradorContext);
+  const { setItems, filters, setFilters } = useContext(ExploradorContext);
 
   useEffect(() => {
     const fetchItems = async () => {
       const items = await getAnimes();
       setItems(items.data);
-      console.log("pasa");
     };
 
     fetchItems();
-  }, [orden]);
+  }, [filters.orden]);
 
   return (
     <div className="row align-items-center mt-3 mt-md-0">
-      <div className="col-md-9 text-start text-lg-end">Ordenar por</div>
-      <div className="col-md-3">
+      <div className="col-lg-10 text-start text-lg-end">Ordenar por</div>
+      <div className="col-lg-2">
         <select
           className="form-select bg-dark text-white mt-2 mt-md-0"
-          onChange={(e) => setOrden(e.target.value)}
+          onChange={(e) => setFilters({ ...filters, orden: e.target.value })}
         >
-          <option value="title">Titulo</option>
           <option value="estreno">Fecha</option>
+          <option value="title">Titulo</option>
         </select>
       </div>
     </div>
