d0c9b1af2c94d34321ac27da8dd93502dba1a922
Tony <anthonymessagesab@gmail.com>
2023-03-01 16:57:48 -0400

    cambios en el nav

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,36 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { RiHome4Fill } from "react-icons/ri";
 import { FaBars, FaUserAlt } from "react-icons/fa";
 import { BsDiscord } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { LoginRegisterModal } from "../LoginRegisterModal/LoginRegisterModal";
-import jwt_decode from "jwt-decode";
 import { MobileMenu } from "../MobileMenu/MobileMenu";
-import { useState } from "react";
 import { Search } from "../Search/Search";
-import { getUserById } from "../../Api/User/user";
+import { useAuth } from "../../hooks/useAuth";
+import { UserNav } from "./UserNav/UserNav";
 
 export const Navbar = () => {
   const [formType, setFormType] = useState("login");
-  const [show, setShow] = useState(false);
-  const [user, setUser] = useState([]);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      const decoded = jwt_decode(token);
-      if (decoded.exp * 1000 < Date.now()) {
-        localStorage.removeItem("token");
-      }
-      getUserById(decoded.id).then((res) => {
-        if (res) {
-          setUser(res);
-          setShow(true);
-        }
-      });
-    }
-  }, []);
+  const { user } = useAuth();
 
   return (
     <>
@@ -60,36 +42,7 @@ export const Navbar = () => {
                   <h2 className="navbar-text-explorer">Explorar</h2>
                 </div>
               </Link>
-              {show ? (
-                <Link
-                  to={`/perfil/${user.id}`}
-                  className="nav-list-link d-none d-sm-block"
-                >
-                  <div className="navbar-container__icons__explore">
-                    <img
-                      className="
-                    navbar-container__icons__explore__img
-                    "
-                      src={user.avatar}
-                      alt="user.name"
-                    />
-                  </div>
-                </Link>
-              ) : (
-                <div className="nav-list-link  d-none d-sm-block">
-                  <div className="navbar-container__icons__home">
-                    <FaUserAlt className="navbar-container__icons_home__icon" />
-                  </div>
-                  <button
-                    type="button"
-                    className="text-decoration-none btn p-0"
-                    data-bs-toggle="modal"
-                    data-bs-target="#modal"
-                  >
-                    <h2 className="navbar-text-explorer">Login</h2>
-                  </button>
-                </div>
-              )}
+              <UserNav user={user} />
             </div>
           </div>
           <div className="col-3 d-flex justify-content-end align-items-center">
