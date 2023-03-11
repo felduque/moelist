39620844ee8a435997efc584ca958b963c4468c4
Tony <anthonymessagesab@gmail.com>
2023-03-11 18:37:51 -0400

    cierra el modal cuando uno se loguea

--- client/src/Components/LoginRegisterModal/LoginRegisterModal.jsx
+++ client/src/Components/LoginRegisterModal/LoginRegisterModal.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import { LoginForm } from "../LoginForm/LoginForm";
 import { IoIosClose } from "react-icons/io";
 import "./LoginRegisterModal.css";
 import { RegisterForm } from "../RegisterForm/RegisterForm";
 
 export const LoginRegisterModal = ({ formType, setFormType }) => {
+  const modalRef = useRef(null);
   return (
     <div
       className="modal fade"
@@ -16,15 +17,18 @@ export const LoginRegisterModal = ({ formType, setFormType }) => {
         <div className="modal-content bg-dark text-white">
           <div className="modal-header ">
             <h5 className="modal-title fs-2 form-title">{formType}</h5>
-            <IoIosClose
-              type="button"
+            <button
+              ref={modalRef}
               className="btn p-0 text-white fs-1"
+              id="modal-close"
               data-bs-dismiss="modal"
               aria-label="Close"
-            />
+            >
+              <IoIosClose />
+            </button>
           </div>
           {formType === "login" ? (
-            <LoginForm setFormType={setFormType} />
+            <LoginForm setFormType={setFormType} modalRef={modalRef} />
           ) : (
             <RegisterForm setFormType={setFormType} />
           )}
