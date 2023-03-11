8b1dd533403f0cef233f1c2fcbcf113acab7e2a3
Tony <anthonymessagesab@gmail.com>
2023-03-11 19:04:27 -0400

    agrega links al searchItem

--- client/src/Components/Search/SearchItem.jsx
+++ client/src/Components/Search/SearchItem.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Search.css";
 
-export const SearchItem = ({ id, image, title, description }) => {
+import { Link } from "react-router-dom";
+
+export const SearchItem = ({ id, image, title, description, contentType }) => {
   return (
     <div className="row mb-3">
-      <div className="col-3">
+      <Link to={`/${contentType}/${id}`} className="col-3">
         <img className="img-fluid search-img" src={image} alt={title} />
-      </div>
-      <div className="col-9 text-white text-start">
-        <h5 className="fw-bold">{title}</h5>
+      </Link>
+      <div to={`/${contentType}/${id}`} className="col-9 text-white text-start">
+        <Link className="fw-bold">{title}</Link>
         <p className="search_desc">{description}</p>
       </div>
     </div>
