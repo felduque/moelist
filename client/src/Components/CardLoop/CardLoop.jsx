60c8fa7d11858174e4ce2683ef270c1b4153a1b7
Tony <anthonymessagesab@gmail.com>
2023-03-17 17:32:35 -0400

    arreglos para responsividad del sidebar

--- client/src/Components/CardLoop/CardLoop.jsx
+++ client/src/Components/CardLoop/CardLoop.jsx
@@ -8,7 +8,7 @@ export const CardLoop = ({
   action = "add",
 }) => {
   const colsStyles = oneCol
-    ? "row-cols-1"
+    ? "row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-1"
     : "row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xxl-6";
 
   return (
