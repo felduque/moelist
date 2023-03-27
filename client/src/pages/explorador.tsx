aa67b204ec5f3f079f2379b5d4f7cd403f0bd432
Tony <anthonymessagesab@gmail.com>
2023-03-27 19:35:47 -0400

    paginacion para explorador

--- client/src/pages/explorador.tsx
+++ client/src/pages/explorador.tsx
@@ -3,19 +3,41 @@ import { ExploradorOrder } from "@/components/Explorador/ExploradorOrder/Explora
 import { ExploradorSearch } from "@/components/Explorador/ExploradorSearch/ExploradorSearch.";
 import { ExploradorSidebar } from "@/components/Explorador/ExploradorSidebar/ExploradorSidebar";
 import { LoadingOverlay } from "@/components/LoadingOverlay/LoadingOverlay";
+import { Pagination } from "@/components/Pagination/Pagination";
+import { search } from "@/utils/api/search";
 import { ExploradorContext } from "@/utils/context/ExploradorContext";
-import { ContentType } from "@/utils/types";
+import { ContentType, FiltersType } from "@/utils/types";
 import React, { useState } from "react";
 
-//loadingOverlay
+const initFilters = {
+  type: "Anime",
+  demography: "",
+  status: "",
+  genres: [],
+};
 
 const Explorador = () => {
   const [items, setItems] = useState<ContentType[]>([]);
   const [loading, setLoading] = useState(true);
+  const [count, setCount] = useState(0);
+  const [filters, setFilters] = useState<FiltersType>(initFilters);
+
+  const handlePageChange = async (selected: number) => {
+    const { data } = await search(filters, 18, selected + 1);
+    setItems(data.result);
+  };
 
   return (
     <ExploradorContext.Provider
-      value={{ setItems, items, setLoading, loading }}
+      value={{
+        setItems,
+        items,
+        setLoading,
+        loading,
+        filters,
+        setFilters,
+        setCount,
+      }}
     >
       <LoadingOverlay loading={loading}>
         <section className="container-fluid bg-dark px-4 px-md-5 pb-5">
@@ -30,7 +52,13 @@ const Explorador = () => {
               <div className="mb-4">
                 <ExploradorOrder />
               </div>
-              <CardLoop cards={items.slice(0, 18)} />
+              <CardLoop cards={items} />
+              <Pagination
+                totalItems={count}
+                itemsPerPage={18}
+                onPageChange={handlePageChange}
+                pages={Math.ceil(count / 18)}
+              />
             </div>
           </div>
         </section>
